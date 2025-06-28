78857e2bfed454d4cd421c0e4fca176ff6270a81
stonedjjh <danjim82@gmail.com>
2025-06-28 15:18:26 -0400

    feat(java): Primer programa: Hola Mundo y práctica
    Creación del primer programa en Java que imprime "Hola Mundo" en la consola.
    Este commit incluye la estructura básica del proyecto, archivos de configuración de IntelliJ IDEA y un archivo .gitignore para evitar subir archivos innecesarios.
    Además, se ha añadido un segundo proyecto de ejemplo con la misma estructura.
    Corresponde a la primera clase del curso de Java.

---
+++ Ejercicios/PrimeraClase/src/PrimeraClase.java
@@ -0,0 +1,11 @@
+/*¡Ahora te toca a ti! Sigue el mismo procedimiento que hice en clase,
+creando un proyecto Java en IntelliJ que debe contener una clase llamada
+"PrimeraClase" con el método main. Al ejecutarlo,deberá imprimir en la consola el texto
+"¡Concluí la aula 01 y ahora estoy sumergiéndome en Java!". */
+
+
+public class PrimeraClase {
+    public static void main(String[] args){
+        System.out.println("¡Concluí la aula 01 y ahora estoy sumergiéndome en Java!");
+    }
+}
