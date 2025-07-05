9a91e2a707f54935fa62c61d7e13cd2c4cf4ba5c
stonedjjh <danjim82@gmail.com>
2025-07-05 08:51:35 -0400

    feat(api): Implementa consumo basico de API con HttpClient y HttpRequest
    Este commit cubre los fundamentos de la comunicacion HTTP en Java, incluyendo:
    - Creacion y configuracion de HttpClient.
    - Construccion de HttpRequest con el patron Builder.
    - Envio de peticiones GET y manejo de HttpResponse como String.
    - Integracion con la API de OMDb.
    - Manejo de API Key usando variables de entorno para seguridad.
    - Creacion del PrincipalConBusqueda para pruebas.

---
+++ screenmatch/src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
@@ -0,0 +1,27 @@
+package com.aluracursos.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalConBusqueda {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner lectura = new Scanner(System.in);
+        System.out.println("Escriba el nombre de una pelicula: ");
+        var busqueda = lectura.nextLine();
+
+        String clave = System.getenv("OMDB_API_KEY"); ;//ingresa aqui tu clave
+        String direccion = "https://www.omdbapi.com/?t="+busqueda+"&apikey="+clave;
+
+        HttpClient client = HttpClient.newHttpClient(); // Creamos un cliente HTTP para enviar peticiones
+        HttpRequest request = HttpRequest.newBuilder() // Iniciamos la construccion de la peticion HTTP
+                .uri(URI.create(direccion)) // Definimos la URL a la que se enviara la peticion
+                .build(); // Finalizamos la construccion de la peticion
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString()); // Enviamos la peticion y esperamos la respuesta como un String
+        System.out.println(response.body()); // Imprimimos el cuerpo de la respuesta en consola
+    }
+}
