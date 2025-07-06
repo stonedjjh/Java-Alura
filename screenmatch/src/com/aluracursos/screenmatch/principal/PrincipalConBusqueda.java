0746505df39d1fa99576be82226439ff96766b77
stonedjjh <danjim82@gmail.com>
2025-07-05 22:38:18 -0400

    feat(files): Implementa persistencia de datos en archivos
    Este commit cubre el manejo de archivos y la serializacion a JSON:
    - Uso de clases java.io como FileWriter y FileReader para escribir y leer archivos.
    - Persistencia de objetos Java a formato JSON en archivos usando Gson.
    - Aplicacion de pretty printing para formato JSON legible.
    - Importancia de cerrar recursos (ej. FileWriter.close()) para evitar fugas y asegurar la escritura.

--- screenmatch/src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
+++ screenmatch/src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
@@ -7,48 +7,68 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrincipalConBusqueda {
         public static void main(String[] args) throws IOException, InterruptedException {
             Scanner lectura = new Scanner(System.in);
-            System.out.println("Escriba el nombre de una pelicula: ");
-            var busqueda = lectura.nextLine();
-            String clave = System.getenv("OMDB_API_KEY"); ;//ingresa aqui tu clave
-            String direccion = "https://www.omdbapi.com/?t="+ busqueda.replace(" ", "+")+"&apikey="+clave;
+            List<Titulo> titulos = new ArrayList<>();
+            Gson gson = new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                    .setPrettyPrinting()
+                    .create();
+            String clave = System.getenv("OMDB_API_KEY");
+            ;//ingresa aqui tu clave
 
-            try{
-                HttpClient client = HttpClient.newHttpClient();
-                HttpRequest request = HttpRequest.newBuilder()
-                        .uri(URI.create(direccion))
-                        .build();
-                HttpResponse<String> response = client
-                        .send(request, HttpResponse.BodyHandlers.ofString());
 
-                String json = response.body();
-                System.out.println(json);
+            while (true) {
 
-                Gson gson = new GsonBuilder()
-                        .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                        .create();
-                TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-                System.out.println(miTituloOmdb);
 
-                Titulo miTitulo = new Titulo(miTituloOmdb);
-                System.out.println("Titulo ya convertido: " + miTitulo);
-            }catch (NumberFormatException e){
-                System.out.println("Ocurrió un error: ");
-                System.out.println(e.getMessage());
-            }catch(IllegalArgumentException e){
-                System.out.println("Error en la URI, verifique la dirección.");
-            }catch (ErrorEnConversionDeDuracionException e){
-                System.out.println(e.getMessage());
+                System.out.println("Escriba el nombre de una pelicula: ");
+                var busqueda = lectura.nextLine();
+                String direccion = "https://www.omdbapi.com/?t=" + busqueda.replace(" ", "+") + "&apikey=" + clave;
+
+                try {
+
+                    if(busqueda.equalsIgnoreCase("salir")){
+                        break;
+                    }
+                    HttpClient client = HttpClient.newHttpClient();
+                    HttpRequest request = HttpRequest.newBuilder()
+                            .uri(URI.create(direccion))
+                            .build();
+                    HttpResponse<String> response = client
+                            .send(request, HttpResponse.BodyHandlers.ofString());
+
+                    String json = response.body();
+                    System.out.println(json);
+
+                    TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                    System.out.println(miTituloOmdb);
+
+                    Titulo miTitulo = new Titulo(miTituloOmdb);
+                    System.out.println("Titulo ya convertido: " + miTitulo);
+                    titulos.add(miTitulo);
+                } catch (NumberFormatException e) {
+                    System.out.println("Ocurrió un error: ");
+                    System.out.println(e.getMessage());
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Error en la URI, verifique la dirección.");
+                } catch (ErrorEnConversionDeDuracionException e) {
+                    System.out.println(e.getMessage());
+                }
             }
+            FileWriter escritura = new FileWriter("titulos.json");
+            escritura.write(gson.toJson(titulos));
+            escritura.close();
             System.out.println("Finalizó la ejecución del programa!");
         }
 }
