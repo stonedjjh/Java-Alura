1973fd611252b8d58e7d7063d3d5e392879f4dec
stonedjjh <danjim82@gmail.com>
2025-07-08 22:57:55 -0400

    feat: Exploración avanzada de Streams y Estadísticas con Optional y DoubleSummaryStatistics
    Este commit encapsula el aprendizaje del cuarto módulo.
    Se exploró el uso de la función peek para la depuración en Streams,
    y se profundizó en operaciones intermedias y terminales como map, ilter y ind.
    
    También se examinó el uso de Optional como contenedor para manejar datos de forma segura y evitar nulos.
    Finalmente, se aprendió sobre la importancia del filtrado de datos para análisis precisos y
    cómo utilizar DoubleSummaryStatistics para generar métricas como calificaciones máximas, mínimas y conteos.

--- screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -9,10 +9,7 @@ import com.aluracursos.screenmatch.service.ConvierteDatos;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
@@ -58,7 +55,11 @@ public class Principal {
         System.out.println("Mostrando los 5 capitulos mejor evaluados");
         datosEpisodios.stream()
                 .filter(e->!e.evaluacion().equalsIgnoreCase("N/A"))
+                .peek(e -> System.out.println("Primer Filtro (N/A)" + e))
                 .sorted(Comparator.comparing(DatosEpisodio::evaluacion).reversed())
+                .peek(e -> System.out.println("Segundo Filtro (M>m)" + e))
+                .map(e-> e.titulo().toUpperCase())
+                .peek(e -> System.out.println("Tercer Filtro Mayusculas" + e))
                 .limit(5)
                 .forEach(System.out::println);
 
@@ -67,7 +68,7 @@ public class Principal {
                 .flatMap(t->t.episodios().stream()
                 .map(d->new Episodio(t.numero(),d)))
                 .collect(Collectors.toList());
-        episodios.forEach(System.out::println);
+/*        episodios.forEach(System.out::println);
 
         // Busqueda de episodios a partir de x año
         System.out.println("Por favor indicar el año a partir del cual deseas ver los episodios");
@@ -84,8 +85,30 @@ public class Principal {
                         "Temporada " + e.getTemporada() +
                                 "Episodio " + e.getTitulo() +
                                 "Fecha de Lanzamiento " + e.getFechaDeLanzamiento().format(dtf)
-                ));
+                ));*/
+        //Busca episodios por pedazo del titulo
+    /*    System.out.println("Por favor escriba el titulo del episodio que desea ver");
+        var pedazoTitulo = teclado.nextLine();
+        Optional<Episodio> episodioBuscado = episodios.stream()
+                .filter(e -> e.getTitulo().toUpperCase().contains(pedazoTitulo.toUpperCase()))
+                .findFirst();
+        if(episodioBuscado.isPresent()){
+            System.out.println(episodioBuscado);
+            System.out.println("Los datos son: " + episodioBuscado.get());
+        }else{
+            System.out.println("Episodio no encontrado");
+        }*/
 
+        Map<Integer, Double> evaluacionesPorTemporadas = episodios.stream()
+                .filter(e -> e.getEvaluacion() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada,
+                        Collectors.averagingDouble(Episodio::getEvaluacion)));
+        System.out.println(evaluacionesPorTemporadas);
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getEvaluacion() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getEvaluacion));
+        
 
     }
 
