c0d0927c495412767a26cecc742c33f7b5b6ce88
stonedjjh <danjim82@gmail.com>
2025-07-13 21:47:12 -0400

    feat: Aula 4 - JSON Web Token (JWT)
    En esta clase, profundizamos en la autenticación con JWT y aprendimos a:
    - Añadir la biblioteca Auth0 java-jwt como dependencia al proyecto.
    - Utilizar esta biblioteca para generar tokens JWT en la API.
    - Inyectar propiedades desde el archivo pplication.properties en clases gestionadas por Spring, usando la anotación @Value.
    - Devolver un token JWT generado desde la API una vez que un usuario se autentica exitosamente.

--- api/src/main/java/med/voll/api/controller/AutenticacionController.java
+++ api/src/main/java/med/voll/api/controller/AutenticacionController.java
@@ -2,6 +2,9 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DatosAutenticacion;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.DatosTokenJWT;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,13 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutenticacionController {
 
     @Autowired
+    private TokenService tokenService;
+
+
+    @Autowired
     private AuthenticationManager manager;
+
 
     @PostMapping
     public ResponseEntity iniciarSesion(@RequestBody @Valid DatosAutenticacion datos) {
-        var token = new UsernamePasswordAuthenticationToken(datos.login(), datos.contrasena());
-        var authenticaon = manager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(datos.login(), datos.contrasena());
+        var autenticacion = manager.authenticate(authenticationToken);
 
-        return ResponseEntity.ok().build();
+        var tokenJWT = tokenService.generarToken((Usuario) autenticacion.getPrincipal());
+
+        return ResponseEntity.ok(new DatosTokenJWT(tokenJWT));
     }
+
 }
---
+++ api/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,39 @@
+package med.voll.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.domain.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    //@Value(staticConstructor = "${api.security.token.secret}")
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generarToken(Usuario usuario) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("API Voll.med")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(fechaExpiracion())
+                    .sign(algoritmo);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("error al generar el  token jwt", exception);
+        }
+    }
+
+    private Instant fechaExpiracion() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-04:00"));
+    }
+
+}
