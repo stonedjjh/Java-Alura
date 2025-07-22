5094117aad6699d8db4e93a0fcadb96e8759823d
stonedjjh <danjim82@gmail.com>
2025-07-21 21:29:41 -0400

    ¡Excelente, Daniel! Has terminado la Aula 4: Test Automatizados. La habilidad de escribir tests es crucial para asegurar la calidad y el buen funcionamiento de tu API.
    Resumen para Commit
    Aquí tienes un resumen conciso para tu próximo commit:
    
    feat: Test Automatizados
    
    Esta aula se enfocó en la implementación de pruebas automatizadas para asegurar la calidad del código en una aplicación Spring Boot.
    
    Se lograron los siguientes puntos:
    - Escritura de tests automatizados para el proyecto, cubriendo diferentes capas de la aplicación.
    - Implementación de tests para interfaces Repository, utilizando la misma base de datos de la aplicación y gestionando perfiles de Spring (@ActiveProfiles) con pplication-test.properties.
    - Desarrollo de tests automatizados para clases Controller, empleando MockMvc para simular peticiones HTTP y validar el comportamiento de la API.
    - Aprendizaje y aplicación de estrategias para testear escenarios de error (código 400) y de éxito (código 200) en los tests de controladores.
    
    Estos tests garantizan la fiabilidad y estabilidad de las funcionalidades implementadas.

---
+++ api/src/test/java/med/voll/api/controller/MedicoControllerTest.java
@@ -0,0 +1,102 @@
+package med.voll.api.controller;
+
+import med.voll.api.domain.direccion.DatosDireccion;
+import med.voll.api.domain.direccion.Direccion;
+import med.voll.api.domain.medico.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+class MedicoControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private JacksonTester<DatosRegistroMedico> datosRegistroMedicoJson;
+
+    @Autowired
+    private JacksonTester<DatosDetalleMedico> datosDetalleMedicoJson;
+
+    @MockBean
+    private MedicoRepository repository;
+
+    @Test
+    @DisplayName("Debería devolver código http 400 cuando las informaciones son inválidas")
+    @WithMockUser
+    void registrar_escenario1() throws Exception {
+        var response = mvc
+                .perform(post("/medicos"))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("Debería devolver código http 200 cuando las informaciones son válidas")
+    @WithMockUser
+    void registrar_escenario2() throws Exception {
+        var datosRegistro = new DatosRegistroMedico(
+                "Medico",
+                "medico@voll.med",
+                "61999999999",
+                "123456",
+                Especialidad.CARDIOLOGIA,
+                datosDireccion());
+
+        when(repository.save(any())).thenReturn(new Medico(datosRegistro));
+
+
+
+        var response = mvc
+                .perform(post("/medicos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(datosRegistroMedicoJson.write(datosRegistro).getJson()))
+                .andReturn().getResponse();
+
+        var datosDetalle= new DatosDetalleMedico(
+                null,
+                datosRegistro.nombre(),
+                datosRegistro.email(),
+                datosRegistro.documento(),
+                datosRegistro.telefono(),
+                datosRegistro.especialidad(),
+                new Direccion(datosRegistro.direccion())
+        );
+        var jsonEsperado = datosDetalleMedicoJson.write(datosDetalle).getJson();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
+    }
+
+    private DatosDireccion datosDireccion() {
+        return new DatosDireccion(
+                "calle x",
+                "distrito y",
+                "ciudad z",
+                "123",
+                "1",
+                "1234",
+                "Caracas"
+        );
+    }
+}
